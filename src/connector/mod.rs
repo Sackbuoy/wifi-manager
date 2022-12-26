322b5e5303572f6c2ae273d97658df4c6a63bdbd
Sackbuoy <noremackientz@gmail.com>
2022-12-26 02:06:46 -0600

    Initial commit

---
+++ src/connector/mod.rs
@@ -0,0 +1,79 @@
+extern crate wifi_rs;
+
+use clap::Parser;
+use clap::Subcommand;
+use simple_error::SimpleError;
+use std::error::Error;
+use wifi_rs::prelude::*;
+use wifi_rs::WiFi;
+
+use nix::ifaddrs::getifaddrs;
+
+#[derive(Parser, Debug)]
+pub struct Args {
+    #[command(subcommand)]
+    pub action: Action,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+pub enum Action {
+    Scan {
+        #[arg(short, long)]
+        interface: String,
+    },
+    Connect {
+        #[arg(default_value_t = get_default_interface().unwrap(), short, long)]
+        interface: String,
+
+        #[arg(short, long)]
+        network: String,
+
+        #[arg(short, long)]
+        password: String,
+    },
+    Disconnect {
+        #[arg(default_value_t = get_default_interface().unwrap(), short, long)]
+        interface: String,
+    },
+}
+
+pub fn connect(
+    interface: String,
+    network: &String,
+    password: String,
+) -> Result<bool, WifiConnectionError> {
+    let config = Some(Config {
+        interface: Some(interface.as_str()),
+    });
+
+    let mut wifi = WiFi::new(config);
+
+    wifi.connect(&network, &password)
+}
+
+pub fn disconnect(interface: String) -> Result<bool, WifiConnectionError> {
+    let config = Some(Config {
+        interface: Some(interface.as_str()),
+    });
+
+    let wifi = WiFi::new(config);
+
+    wifi.disconnect()
+}
+
+fn get_default_interface() -> Result<String, Box<dyn Error>> {
+    let addrs = getifaddrs()?;
+
+    // I dont know a better way to find the default rn so this is what we doin
+    let common_iface_names = vec!["eth0", "eth1", "wlan0", "enp3s0", "wlp2s0"];
+
+    for ifaddr in addrs {
+        if common_iface_names.contains(&ifaddr.interface_name.as_str()) {
+            return Ok(ifaddr.interface_name);
+        }
+    }
+
+    Err(Box::new(SimpleError::new(
+        "Could not find network interface. Place specify using the -i flag",
+    )))
+}
---
+++ src/main.rs
@@ -0,0 +1,82 @@
+mod connector;
+
+use clap::Parser;
+use connector::{Action, Args};
+use simple_error::SimpleError;
+use std::error::Error;
+use std::fmt;
+use std::str::FromStr;
+
+#[derive(Debug)]
+enum Command {
+    Scan,
+    Connect,
+    Disconnect,
+}
+
+impl fmt::Display for Command {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Command::Scan => write!(f, "scan"),
+            Command::Connect => write!(f, "connect"),
+            Command::Disconnect => write!(f, "disconnect"),
+        }
+    }
+}
+
+impl FromStr for Command {
+    type Err = SimpleError;
+
+    fn from_str(input: &str) -> Result<Command, Self::Err> {
+        match input.to_lowercase().as_str() {
+            "scan" => Ok(Command::Scan),
+            "connect" => Ok(Command::Connect),
+            "disconnect" => Ok(Command::Disconnect),
+            _ => Err(SimpleError::new(format!(
+                "Invalid command given: {}",
+                input.to_lowercase().as_str()
+            ))),
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+
+    match args.action {
+        Action::Scan { interface: _ } => Ok(()),
+        Action::Connect {
+            interface,
+            network,
+            password,
+        } => {
+            match connector::connect(interface, &network, password) {
+                Ok(success) => {
+                    if success {
+                        println!("Connected to {}", network);
+                    } else {
+                        println!("Could not connect, but no error was reported")
+                    }
+                }
+                Err(e) => println!("Could not connect: {:?}", e),
+            }
+
+            Ok(())
+        }
+        Action::Disconnect { interface } => {
+            match connector::disconnect(interface) {
+                Ok(success) => println!(
+                    "{}",
+                    if success {
+                        "Disconnected from network"
+                    } else {
+                        "Could not disconnect, but no error was reported"
+                    }
+                ),
+                Err(e) => println!("Could not disconnect: {:?}", e),
+            }
+
+            Ok(())
+        }
+    }
+}
